41b2535660568fdb5f050d01a6692b0bac10c8fc
pritam-rgb <pritamchatterjee945@gmail.com>
2020-06-10 00:07:44 +0530

    After setup of database

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+
+// Requires the libary to connect to database
+const mongoose= require('mongoose');
+
+// To connect with the database
+mongoose.connect('mongodb://localhost/codial_devolopment');
+
+// To check wether connection is being made
+const db= mongoose.connection;
+// If error
+db.on('error',console.error.bind(console,'error connecting to db'));
+// If succesfull
+db.once('open',function(){
+    console.log('Succesfully connected to the database');
+});
+
+module.exports= db;
--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const app= express();
 
 const port=8000;
 
+const expressLayouts= require('express-ejs-layouts');
+const db= require('./config/mongoose');
+
+app.use(express.static('./asset'));
+
+app.use(expressLayouts);
+// To have diffrent styles and script on pages renderd via layout design 
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+
+
+
+
+
+
+
 // To use the express router
 
 app.use('/',require('./router'));
