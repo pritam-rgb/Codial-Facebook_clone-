bca4b4eae997957eec17904d5282336e0831c3fc
pritam-rgb <pritamchatterjee945@gmail.com>
2020-07-25 01:27:33 +0530

    Added cookie for sign in up

--- controller/userController.js
+++ controller/userController.js
@@ -3,15 +3,23 @@ module.exports.profile= function(req,res){
         title:'User Profile'
     });
 };
-
+// To render the Sign-in page
 module.exports.sign_in= function(req,res){
     return res.render('user_sign_in',{
         title:'Sign In'
     })
 }
-
+// To render the sign-up page
 module.exports.sign_up= function(req,res){
     return res.render('user_sign_up',{
         title:'Sign Up'
     })
 }
+
+module.exports.create= function(req,res){
+// ToDO later
+};
+
+module.exports.create_session= function(req,res){
+    //TODO later
+};
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express= require('express');
+const cookieParser= require('cookie-parser');
 
 const app= express();
 
@@ -7,6 +8,10 @@ const port=8000;
 const expressLayouts= require('express-ejs-layouts');
 const db= require('./config/mongoose');
 
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 app.use(express.static('./asset'));
 
 app.use(expressLayouts);
